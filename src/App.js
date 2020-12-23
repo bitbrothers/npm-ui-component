2297a90524c0f0fa656ea297e9705b4fe65d180f
Krish Vora <Krishvora007@gmail.com>
2020-12-23 11:36:34 +0530

    Alert and Badge Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import React from 'react';
+import Alert from './components/bootstrap/Alert/Alert';
+import Badge from './components/bootstrap/Badge/Badge';
+import Aux from './hoc/Aux'
+
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <Aux>
+        <Alert />
+        <Badge />
+      </Aux>
+    );
   }
 }
 
---
+++ src/components/bootstrap/Badge/Badge.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Badge() {
+    return <span class="badge badge-danger">New</span>
+}
+
+export default Badge;
