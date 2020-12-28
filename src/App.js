0b45ae29cae97090365876dcfd6fe112f64a1aad
Krish Vora <Krishvora007@gmail.com>
2020-12-28 16:40:53 +0530

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Aux from './hoc/Aux';
+import $ from 'jquery';
 
 //Bootstrap Components
 import Alert from './components/bootstrap/Alert';
@@ -54,11 +55,82 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount(){
+    this.sidebarCollapse();
+    this.menu();
+    this.dropdown();
+    this.carousel('.js-carousel-item-pages','.js-carousel-first', 3000);
+    this.carousel('.js-carousel-item-component-left','.js-carousel-second', 3000);
+    this.carousel('.js-carousel-item-component-right','.js-carousel-third', 4000);
+  }
+
+  sidebarCollapse(){
+    $('.js-btn-collaspe').on('click', function(){
+      $('.js-sidebar-collapse').toggleClass('left-sidebar__collapse--visible');
+  
+      $('.overlay-mobile').fadeToggle('200');
+  
+      $('.overlay-mobile').on('click', function(){         
+        $('.js-sidebar-collapse').removeClass('left-sidebar__collapse--visible');
+        $(this).fadeOut();
+      });
+    })
+  }
+ 
+  //Menu 
+  menu(){
+    $('.js-menu-item').each(function () {
+      $(this).children('.js-menulink').on('click', function () {
+        var menuItem = $(this).parent('.js-menu-item');
+        if(menuItem.hasClass('main-menu__item--active')) {
+          menuItem.children('.js-submenu').slideUp(250);
+          menuItem.removeClass('main-menu__item--active');
+        }
+  
+        else {
+          menuItem.children('.js-submenu').slideDown(250);
+          menuItem.addClass('main-menu__item--active');
+        }
+      })
+    })
+  }
+
+  //dropdown
+  dropdown(){
+    $('.js-dropdown-toggle').on('click', function(){
+      var dropdown = $(this).parent('.js-dropdown');
+      var droplist =  dropdown.children('.js-dropdown-list');
+      $('.js-dropdown-list').not(droplist).fadeOut(250);
+      droplist.fadeToggle(250);
+    })
+  
+    $(document).on('click', function(event) {
+      if (!$(event.target).closest('.js-dropdown').length) {
+          $(".js-dropdown-list").fadeOut(250);
+      }
+    });
+  }
+ 
+  //carousel 
+  carousel(item, element, time){
+    $(item + ':gt(0)').hide();
+
+    setInterval(function(){
+      $(item+ ':first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo(element);
+    }, time);
+  }
+
   render() {
     return (
       <Aux className="app">
         <Sidebar 
-        logo={logo3}/>
+        logo={logo3}
+        userPhoto={logo}/>
 
 {/*
         <Activity 
--- src/components/menu/Sidebar.js
+++ src/components/menu/Sidebar.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
-function Sidebar({logo}) {
+import Main from './Main';
+function Sidebar({logo, userPhoto}) {
     return (
         <aside class="left-sidebar" id="js-sidebar">
             <div class="logo"><a class="logo__link" href="#">
-                    <h1><img src="images/logo.svg" alt="logo-main" /></h1>
+                    <h1><img src={logo} alt="logo-main" /></h1>
                 </a></div>
             <div class="d-mobile"><button class="btn--collaspe js-btn-collaspe"><span class="btn--collaspe__icon"></span></button>
                 <div class="user-profile js-dropdown">
@@ -20,7 +20,7 @@ function Sidebar({logo}) {
             </div>
 
             <div class="left-sidebar__collapse js-sidebar-collapse">
-
+            <Main />
                 <div class="quick-links"><span class="quick-links__title">Quick Links</span>
                     <ul class="quick-links__list">
                         <li class="quick-links__item"> <a class="quick-links__link">Team Overview</a></li>
@@ -29,7 +29,7 @@ function Sidebar({logo}) {
                         <li class="quick-links__item"> <a class="quick-links__link">Kanban Board</a></li>
                     </ul>
                 </div>
-                <form class="search-form"> <span class="search-form__icon"> <i class="fas fa-search"></i></span><input class="search-form__field" type="text" placeholder="Search" /></form>
+                <form class="search-form"> <span class="search-form__icon"><i class="fas fa-search"></i></span><input class="search-form__field" type="text" placeholder="Search" /></form>
                 <div class="dropdown js-dropdown"><button class="btn--toggle js-dropdown-toggle">Add New</button>
                     <ul class="dropdown__list js-dropdown-list">
                         <li class="dropdown__item"><a class="dropdown__link" href="#">Team</a></li>
@@ -40,7 +40,7 @@ function Sidebar({logo}) {
             </div>
 
             <div class="user-profile js-dropdown">
-                <div class="user-profile__avatar js-dropdown-toggle"><img class="img-fluid" src="images/avatar-male-1.jpg" alt="user-avatar" /></div>
+                <div class="user-profile__avatar js-dropdown-toggle"><img class="img-fluid" src={userPhoto} alt="user-avatar" /></div>
                 <div class="user-profile__setting js-dropdown-list">
                     <ul class="list-setting">
                         <li class="list-setting__item"><a class="list-setting__link" href="#">Profile</a></li>
