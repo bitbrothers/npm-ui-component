85a1d2cc80c0734fb4b92cfd7fa9bd0bdd9ba8ad
Krish Vora <Krishvora007@gmail.com>
2021-01-06 16:53:39 +0530

    Sidebar

--- src/components/menu/Search.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Search() {
-    return (
-        <form class="search-form"><span class="search-form__icon"><i class="fas fa-search"></i></span><input class="search-form__field" type="text" placeholder="Search" /></form>
-    )
-}
-
-export default Search
