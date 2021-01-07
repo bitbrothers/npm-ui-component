133cdd63e256d48f0ef852325e543dd47f47dcaf
Krish Vora <Krishvora007@gmail.com>
2021-01-07 15:58:49 +0530

    Enabled Tab switching

--- src/components/bootstrap/Nav.js
+++ src/components/bootstrap/Nav.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 function Nav() {
     return (
-        <ul class="nav nav-tabs nav-fill">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-        </ul>
+            <ul class="nav nav-tabs nav-fill" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active" data-toggle="tab" href="#tasks" role="tab" aria-controls="tasks" aria-selected="true">Tasks</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#files" role="tab" aria-controls="files" aria-selected="false">Files</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#activity" role="tab" aria-controls="activity" aria-selected="false">Activity</a>
+                </li>
+            </ul>
     )
 }
 
